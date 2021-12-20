dd0ee6837dca0ab5d046627c24b82e7b2e773a0a
shani <shhhaniburde@gmail.com>
2021-12-20 18:47:57 -0500

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+
+    const form = document.querySelector('form')
+
+    form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    const input = document.querySelector("#searchByID")
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(resp => resp.json())
+        .then(data => {
+
+            const h4 = document.querySelector('h4')
+            h4.textContent = data.title
+
+                const p = document.querySelector('p')
+            p.textContent = data.summary
+        
+          })
+        
+    })
+
 }
 
-document.addEventListener('DOMContentLoaded', init);
+const form = document.addEventListener('DOMContentLoaded', init)
